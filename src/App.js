140bf40f8a03406c84b74789231dc3ab4b7c9ad2
mohamed.kedir <a6002651@bestbuy.com>
2019-10-17 19:29:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+// import Countdown from 'react-countdown-now';
+// import Sidebar from "react-sidebar";
+// import MenuBar from './images/icons8-menu-30.png';
+import Home from './Home';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export default () => {
+  return <Home name="Alligator" />;
+};
+// class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       sidebarOpen: true
+//     }
+//     this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+//   }
+//   onSetSidebarOpen(open) {
+//     this.setState({ sidebarOpen: open });
+//   }
+//   render(props){
+//     const sideBarButton = this.state.sidebarOpen ? 'opened' : 'closed';
+//   return (
+//     <div>
+
+//     <Sidebar
+//     sidebar={<div className='side-bar'></div>}
+//     open={this.state.sidebarOpen}
+//     onSetOpen={this.onSetSidebarOpen}
+//     styles={{ sidebar: { background: "white" } }}
+//     >
+//     <button className={sideBarButton} onClick={() => this.onSetSidebarOpen(true)}>
+//         <img className='menu-bar' src={MenuBar} alt='menu-icon' />
+//     </button>
+//   </Sidebar>
+//     <div className="App">
+//       <Countdown date={Date.now() + 50001} />
+//     </div>
+//     </div>
+// );
+// }
+// }
+// export default App;
